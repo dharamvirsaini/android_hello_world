cdfd1f0723dcd89d244d8cda3695a18fa208cef0
Dharamvir <dharamvir@HoneyMacbook.local>
2017-05-16 07:34:47 +0530

    base code

---
+++ MyApplication/app/src/main/java/com/example/dharamvir/myapplication/OpenTokConfig.java
@@ -0,0 +1,15 @@
+package com.example.dharamvir.myapplication;
+
+import android.webkit.URLUtil;
+
+public class OpenTokConfig {
+    // *** Fill the following variables using your own Project info from the OpenTok dashboard  ***
+    // ***                      https://dashboard.tokbox.com/projects                           ***
+
+    // Replace with your OpenTok API key
+    public static final String API_KEY = "YOUR_API_KEY";
+    public static final String SESSION_ID = "YOUR_SESSION_ID";
+    public static final String TOKEN = "TOKEN";
+
+
+}
